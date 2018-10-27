e671074f62db8325bb24017f707f41cbf266fab2
AlejandraCasuscelli <alejandracasuscelli@gmail.com>
2018-10-27 17:47:35 -0300

    modf

--- Codigo Fuente/PedidosEmpanadas/Api/Controllers/PedidosController.cs
+++ Codigo Fuente/PedidosEmpanadas/Api/Controllers/PedidosController.cs
@@ -8,18 +8,29 @@ using System.Web.Http;
 using System.Web.Http.Cors;
 using System.Web.Http.Results;
 
+
 namespace Api.Controllers
 {
     //[Route("api/[controller]")]
     //[EnableCors(origins: "http://localhost:57162/", headers: "*", methods: "*")]
     public class PedidosController : ApiController
     {
+
+        //private readonly ServicioPedido _servicioPedido = new ServicioPedido();
+        PedidoRequest pedido = new PedidoRequest();
+        //[HttpPost]
+        //public JsonResult ConfirmarGustos([FromBody] JsonResult pedido)
+        //{
+
+
+        //    return null;
+        //}
+
         [HttpPost]
         public PedidoRequest ConfirmarGustos([FromBody] PedidoRequest pedido)
         {
 
             return null;
         }
-
     }
 }
