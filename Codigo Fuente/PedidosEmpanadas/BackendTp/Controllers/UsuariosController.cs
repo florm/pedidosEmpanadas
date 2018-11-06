39e1fab5e7ac2fbb5ac4c5d88264c249877dbf06
AlejandraCasuscelli <alejandracasuscelli@gmail.com>
2018-11-05 21:30:53 -0300

    Agrego viewmodel de pedido y usuarios controller para manejar sesion y lista

---
+++ Codigo Fuente/PedidosEmpanadas/BackendTp/Controllers/UsuariosController.cs
@@ -0,0 +1,62 @@
+﻿using BackendTp.Models;
+using BackendTp.Servicios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace BackendTp.Controllers
+{
+    public class UsuariosController : ApiController
+    {
+        ServicioUsuario servicioUsuario = new ServicioUsuario();
+        ServicioPedido servicioPedido = new ServicioPedido();
+
+        [ResponseType(typeof(Usuario))]
+        public IHttpActionResult GetMobileUser(string id, string param2)
+        {
+            var usuario = servicioUsuario.LoginMobile(id, param2);
+
+            return Ok(usuario);
+        }
+
+        [ResponseType(typeof(List<PedidoViewModel>))]
+        public IHttpActionResult GetListaPedidos(int id)
+        {
+            List<PedidoViewModel> ListaPrueba = new List<PedidoViewModel>();
+            var pedidos = servicioPedido.Lista(id);
+
+            foreach (Pedido p in pedidos)
+            {
+                ListaPrueba.Add(new PedidoViewModel
+                {
+                    IdPedido = p.IdPedido,
+                    IdUsuarioResponsable = p.IdUsuarioResponsable,
+                    FechaCreacion = p.FechaCreacion,
+                    NombreNegocio = p.NombreNegocio,
+                    Estado = p.IdEstadoPedido,
+                    Rol = p.IdUsuarioResponsable,
+                    EstadoS = p.EstadoPedido.Nombre
+                });
+            }
+
+            foreach (PedidoViewModel pvm in ListaPrueba)
+            {
+                if (pvm.Rol == id)
+                {
+                    pvm.RolS = "Responsable";
+                }
+                else
+                {
+                    pvm.RolS = "Invitado";
+                }
+
+            }
+
+            return Ok(ListaPrueba);
+        }
+    }
+}
