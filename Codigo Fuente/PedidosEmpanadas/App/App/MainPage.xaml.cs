7a5d103a59cbae6b99878d403dc1786ae6993d1f
AlejandraCasuscelli <alejandracasuscelli@gmail.com>
2018-11-05 21:54:22 -0300

    Se visualiza bien la lista de pedidos

--- Codigo Fuente/PedidosEmpanadas/App/App/MainPage.xaml.cs
+++ Codigo Fuente/PedidosEmpanadas/App/App/MainPage.xaml.cs
@@ -35,8 +35,7 @@ namespace App
 
                 string url = string.Format("/api/Usuarios/GetMobileUser/{0}/{1}", Correo.Text, Pass.Text);
 
-                string url2 = "http://192.168.0.6:45457" + url;
-                var ii = 0;
+                string url2 = "http://192.168.0.6:45455" + url;
 
                 var response = await client.GetAsync(url2);
                 result = response.Content.ReadAsStringAsync().Result;
