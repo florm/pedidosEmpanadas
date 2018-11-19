18752c9194212947dd17c75251da43f965b7c1c0
AlejandraCasuscelli <alejandracasuscelli@gmail.com>
2018-11-19 12:06:44 -0300

    Correcciones en mainpage y modelpedidorequest

---
+++ Codigo Fuente/PedidosEmpanadas/App/App/Models/PedidoRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Models
+{
+    public class PedidoRequest
+    {
+        public List<GustoEmpanadasCantidad> GustoEmpanadasCantidad { get; set; }
+        public int IdUsuario { get; set; }
+        public int IdPedido { get; set; }
+    }
+}
