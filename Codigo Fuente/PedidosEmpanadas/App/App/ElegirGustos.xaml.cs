b6e9cac84d13c66da591c7c07401ade403d09f02
AlejandraCasuscelli <alejandracasuscelli@gmail.com>
2018-11-19 12:04:03 -0300

    Pagina para elegir gustos ok

--- Codigo Fuente/PedidosEmpanadas/App/App/ElegirGustos.xaml.cs
+++ Codigo Fuente/PedidosEmpanadas/App/App/ElegirGustos.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using App.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,10 +15,141 @@ namespace App
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ElegirGustos : ContentPage
 	{
-		public ElegirGustos (int IdPedido)
-		{
-			InitializeComponent ();
+        public int IdPedido { get; set; }
+        List<GustoEmpanadasCantidad> ListaDeGustos = new List<GustoEmpanadasCantidad>();
+        List<Entry> ListaEntries = new List<Entry>();
+        List<Label> ListaLabels = new List<Label>();
 
-		}
-	}
+        public ElegirGustos(PedidosVm pedido)
+        {
+            InitializeComponent();
+
+            NombreNegocio.Text = "Negocio: " + pedido.NombreNegocio;
+            if (pedido.Descripcion != null || pedido.Descripcion != "")
+            {
+                Descripcion.Text = pedido.Descripcion;
+            }
+            else
+            {
+                Descripcion.IsVisible = false;
+            }
+            PrecioDocena.Text = "Precio por docena: $ " + pedido.PrecioDocena;
+            PrecioUnidad.Text = "Precio por unidad: $ " + pedido.PrecioUnidad;
+
+            this.InicializarEntrys(pedido.IdPedido);
+            IdPedido = pedido.IdPedido;
+        }
+
+        public async void InicializarEntrys(int id)
+        {
+            string result;
+            try
+            {
+                HttpClient client = new HttpClient();
+                string url = string.Format("/api/Usuarios/GetListaDeGustosEnPedido/{0}/{1}", id, App.Current.Properties["IdUsuario"]);
+                string url2 = App.UrlApi + url;
+                var response = await client.GetAsync(url2);
+                result = response.Content.ReadAsStringAsync().Result;
+            }
+            catch
+            {
+                await DisplayAlert("Error", "Error de conexion", "Aceptar");
+                return;
+            }
+
+            //var settings = new JsonSerializerSettings
+            //{
+            //    NullValueHandling = NullValueHandling.Ignore,
+            //    MissingMemberHandling = MissingMemberHandling.Ignore
+            //};
+
+            ListaDeGustos = JsonConvert.DeserializeObject<List<GustoEmpanadasCantidad>>(result);
+
+            foreach (GustoEmpanadasCantidad u in ListaDeGustos)
+            {
+                //ContenedorEntrys.Children.Add(new Entry() { Text = u.Email, ClassId = u.IdUsuario.ToString() });
+                ListaEntries.Add(new Entry() { ClassId = u.IdGustoEmpanada.ToString(), Text = u.Cantidad.ToString(), Keyboard = Keyboard.Numeric });
+                ListaLabels.Add(new Label() { Text = u.Nombre, ClassId = u.IdGustoEmpanada.ToString() });
+            }
+
+            foreach (Entry entr in ListaEntries)
+            {
+                foreach (Label lb in ListaLabels)
+                {
+                    if (entr.ClassId == lb.ClassId)
+                    {
+                        ContenedorEntrys.Children.Add(lb);
+                    }
+                }
+                ContenedorEntrys.Children.Add(entr);
+            }
+        }
+
+        private async void BotonReg_Clicked(object sender, EventArgs e)
+        {
+            //Falta validar
+            //string txt = "";
+            //foreach (Entry entr in ListaEntries)
+            //{
+            //    txt = entr.Text;
+            //    if (txt.Contains("."))
+            //    {
+            //        await DisplayAlert("", "Solo se admiten números enteros", "");
+            //        return;
+            //    }
+            //}
+
+            //if (txt.Contains("."))
+            //{
+            //    await DisplayAlert("", "Solo se admiten números enteros", "");
+            //    return;
+            //}
+
+            PedidoRequest pr = new PedidoRequest();
+            foreach (Entry entr in ListaEntries)
+            {
+                foreach (GustoEmpanadasCantidad g in ListaDeGustos)
+                {
+                    if (g.IdGustoEmpanada.ToString() == entr.ClassId)
+                    {
+                        int cant;
+                        int.TryParse(entr.Text, out cant);
+                        g.Cantidad = cant;
+                    }
+                }
+            }
+
+            pr.GustoEmpanadasCantidad = ListaDeGustos;
+            pr.IdPedido = IdPedido;
+            //int id;
+            int.TryParse(App.Current.Properties["IdUsuario"].ToString(), out int id);
+            pr.IdUsuario = id;
+
+            var jsonRequest = JsonConvert.SerializeObject(pr);
+            var content = new StringContent(jsonRequest, Encoding.UTF8, "text/json");
+
+            string result;
+
+            try
+            {
+                HttpClient client = new HttpClient();
+
+                string url = string.Format("/api/Pedidos/ConfirmarGustos");
+                string url2 = App.UrlApi + url;
+
+                var response = await client.PostAsync(url2, content);
+                result = response.Content.ReadAsStringAsync().Result;
+            }
+            catch
+            {
+                await DisplayAlert("Error", "Error de conexion", "Aceptar");
+                return;
+            }
+
+
+            var des = JsonConvert.DeserializeObject<string>(result);
+            await DisplayAlert("", des, "ok");
+            await Navigation.PushAsync(new Lista());
+        }
+    }
 }
--- Codigo Fuente/PedidosEmpanadas/App/App/Models/UserReg.cs
+++ Codigo Fuente/PedidosEmpanadas/App/App/Models/UserReg.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace App.Models
 {
-    class UserReg
+   public class UserReg
     {
         public string Email { get; set; }
         public string Password { get; set; }
